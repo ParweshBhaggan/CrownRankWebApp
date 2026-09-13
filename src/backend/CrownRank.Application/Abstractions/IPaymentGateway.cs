72f9e0b6c68744cc8859168df16a2a94bf4eabd9
Parwesh Bhaggan <parweshbhaggan@outlook.com>
2026-09-14 00:23:03 +0200

    Refreshing Backend

--- src/backend/CrownRank.Application/Abstractions/IPaymentGateway.cs
+++
@@ -1,9 +0,0 @@
-namespace CrownRank.Application.Abstractions;
-
-public sealed record CheckoutRequest(Guid CreatorId, decimal Amount, string Currency, string Purpose, Guid ReferenceId);
-public sealed record CheckoutSession(string Id, bool Confirmed);
-
-public interface IPaymentGateway
-{
-    Task<CheckoutSession> CreateCheckoutAsync(CheckoutRequest request, CancellationToken cancellationToken);
-}
